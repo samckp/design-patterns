0f74739d78f0bb301ebd6959fda095dbe0d3b67d
samckp <sam.mca.liet@gmail.com>
2020-09-07 13:28:05 +0530

    basic Singleton added

--- src/main/java/edu/dp/singleton/SingletonPattern.java
+++ src/main/java/edu/dp/singleton/SingletonPattern.java
@@ -2,5 +2,16 @@ package edu.dp.singleton;
 
 public class SingletonPattern {
 
+    public static void main(String[] args) {
+
+        Singleton sig ;
+
+        sig= Singleton.getInstance();
+        System.out.println(sig);
+
+        sig= Singleton.getInstance();
+        System.out.println(sig);
+
+    }
     
 }
